c571b64837c42ce3bc8e6e590e7045c00fc2bc3c
Namekulero <jmontero.gutierrez2002@gmail.com>
2025-02-03 22:35:28 -0500

    10.35 pm

--- src/app-minesweeper.js
+++ src/app-minesweeper.js
@@ -1,8 +1,8 @@
 let board = [] // Inicializa el tablero como una lista.
 
 const SIZE = { // Inicializa un objeto 'SIZE' con las filas y columnas del tablero.
-    rows: 8, 
-    columns: 8, 
+    rows: 5,
+    columns: 5,
     length: () => SIZE.rows * SIZE.columns // Cantidad de casillas del tablero.
 }
 
@@ -12,7 +12,7 @@ const readLine = require('readline');
 
 let selRow, selCol; // Definición de fila y columna seleccionada 
 let gameOver = false; // Boolean para finalizar el juego
-let possibleMines; // Cuántas minas pueden haber en el tablero desconocido
+let possibleMines; // Minas posibles; representa cuántas casillas faltan por tocar en el juego
 let actualMines; // Cuántas minas realmente hay
 
 const reader = readLine.createInterface({ // Lector de entrada por consola
@@ -39,15 +39,15 @@ async function main() { // Función principal
 
     selRow = parseInt(await askForTile('Fila de casilla inicial: '));
     selCol = parseInt(await askForTile('Columna de casilla inicial: '));
-    possibleMines = SIZE.length;
-    actualMines = 0;
+
     fillMines(board, nMines, selRow - 1, selCol - 1); // Solo se puede hacer el llamado a 
     // llenar minas tras haber seleccionado la primera casilla.
-
     showTiles(board, selRow - 1, selCol - 1); // Muestra la casilla seleccionada.
 
     while (!gameOver) { // Continuación del juego; si sale de este 'while', se acaba el juego.
         try {
+            actualMines = 0;
+            possibleMines = SIZE.length();
             selRow = parseInt(await askForTile('Fila de la casilla: '));
             selCol = parseInt(await askForTile('Columna de la casilla: '));
             showTiles(board, selRow - 1, selCol - 1); // Muestra la casilla seleccionada.
@@ -58,25 +58,18 @@ async function main() { // Función principal
 }
 
 const fill = (matrix, rows, columns) => { // Función de llenado del tablero.
-    for (let i = -1; i < rows; i++) { // Recorre cada fila del tablero.
-        matrix[i+1] = [] // Cada posición inicializada en la lista es otra lista.
-        for (let j = -1; j < columns; j++) { // Recorre cada columna del tablero.
-
-            if (i === -1) {
-                matrix[i+1][j+1] = { tile: j+1 };
-            } else if (j === -1) {
-                matrix[i+1][j+1] = { tile: i+1 };
-            } else {
-                matrix[i+1][j+1] = { adjMines: 0, suspect: false, mine: false, tile: 'X' }; // Llena el tablero con casillas desconocidas.
-            }
+    for (let i = 0; i < rows; i++) { // Recorre cada fila del tablero.
+        matrix[i] = [] // Cada posición inicializada en la lista es otra lista.
+        for (let j = 0; j < columns; j++) { // Recorre cada columna del tablero.
+            matrix[i][j] = { adjMines: 0, suspect: false, mine: false, tile: 'X' }; // Llena el tablero con casillas desconocidas.
         }
     };
 }
 
 const fillMines = (matrix, n, row, col) => { // Función de llenado de casillas de minas.
     for (let i = 0; i < n; i++) {
-        const x = Math.floor(Math.random() * SIZE.rows) // Genera una fila y columna aleatoria para generar una mina ahí.
-        const y = Math.floor(Math.random() * SIZE.columns)
+        const x = Math.floor(Math.random() * (SIZE.rows)) // Genera una fila y columna aleatoria para generar una mina ahí.
+        const y = Math.floor(Math.random() * (SIZE.columns))
         if (x != row && y != col && !matrix[x][y].mine) { // No puede generar una mina en la casilla inicial ni repetir bomba en la casilla.
             matrix[x][y].mine = true; actualMines++;
             for (let xx = x - 1; xx <= x + 1; xx++) {
@@ -89,10 +82,16 @@ const fillMines = (matrix, n, row, col) => { // Función de llenado de casillas 
 }
 
 function print(matrix) { // Función para imprimir el tablero.
-    for (const row of matrix) {
-        let line = ''
-        for (const cell of row) { // Por cada fila de la matriz,
-            line = line + cell.tile + ' ' // imprima cada casilla en la fila.
+    for (let i = -1; i < SIZE.columns; i++) {
+        let line = "";
+        for (let j = -1; j < SIZE.rows; j++) { // Recorre cada casila individual
+            if (i === -1) {
+                line = line + (j + 1) + " ";
+            } else if (j === -1) {
+                line = line + (i + 1) + " ";
+            } else {
+                line = line + matrix[i][j].tile + ' '
+            }
         }
         console.log(line)
     };
@@ -108,7 +107,7 @@ function showTiles(matrix, row, col) {
     } else {
         matrix[row][col].tile = matrix[row][col].adjMines;
         possibleMines--;
-        if (possibleMines == actualMines) {
+        if (possibleMines === actualMines) {
             console.log('\nGANASTE - Fin del juego\n')
             gameOver = true;
         }
